f0b81ad58f7c330e29153e0e7b58144a173dc3ee
Mohsin-4 <59621582+Mohsin-4@users.noreply.github.com>
2020-10-08 22:04:39 -0700

    Update DashBoard.js

--- src/Components/DashBoard.js
+++ src/Components/DashBoard.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React, { useState } from 'react';
 import './Dashboard.css'
 
@@ -62,6 +63,85 @@ function DashBoard(props) {
 
     </div >
   )
+=======
+// import React from 'react'
+
+// function DashBoard() {
+//   return (
+//     <div>
+//      <p>line added</p> 
+//     </div>
+//   )
+// }
+
+// export default DashBoard
+
+import React, { useState } from "react";
+// import DashBoard from './DashBoard';
+
+function DashBoard() {
+  const [image, setImage] = useState({ preview: "", raw: "" });
+
+  const handleChange = e => {
+    if (e.target.files.length) {
+      setImage({
+        preview: URL.createObjectURL(e.target.files[0]),
+        raw: e.target.files[0]
+      });
+    }
+  };
+
+  const handleUpload = async e => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("image", image.raw);
+
+    await fetch("YOUR_URL", {
+      method: "POST",
+      headers: {
+        "Content-Type": "multipart/form-data"
+      },
+      body: formData
+    });
+  };
+
+  return (
+    <div>
+
+    <div>
+      <div>
+        <div>
+          <div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+
+      <label htmlFor="upload-button">
+        {image.preview ? (
+          <img src={image.preview} alt="dummy" width="600" height="600" />
+        ) : (
+          <>
+            <span className="fa-stack fa-2x mt-3 mb-2">
+              <i className="fas fa-circle fa-stack-2x" />
+              <i className="fas fa-store fa-stack-1x fa-inverse" />
+            </span>
+            <h5 className="text-center" style={{cursor:"pointer"}}>Click to Upload your photo</h5>
+          </>
+        )}
+      </label>
+      <input
+        type="file"
+        id="upload-button"
+        style={{ display: "none" }}
+        onChange={handleChange}
+      />
+      <br />
+      <button onClick={handleUpload}>Upload</button>
+    </div>
+  );
+>>>>>>> Stashed changes
 }
 
 export default DashBoard
